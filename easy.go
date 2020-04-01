8ae1bb13e48cee3edb6c704d13a1e8830dc3cc04
unicornikita <nikitakapusin@gmail.com>
2020-04-01 23:13:24 +0200

    apis

--- easy.go
+++ easy.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
+
+	"context"
 
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/messaging"
 	"github.com/gocolly/colly"
-	"golang.org/x/net/context"
 	"google.golang.org/api/option"
 )
 
@@ -19,13 +21,24 @@ type vsebina struct {
 	predmet  string
 	profesor string
 }
-type izbranRazred struct {
-	izbranrazred string
-}
 
 var dnevi [9][6]vsebina = [9][6]vsebina{}
 
+var client *messaging.Client
+var ctx context.Context
+
 func main() {
+	ctx = context.Background()
+	opt := option.WithCredentialsFile("/home/pi/Documents/easy-matura-config.json")
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		fmt.Errorf("error initializing app: %v", err)
+	}
+	if app == nil {
+		fmt.Println("app is nil")
+	}
+
+	client, err = app.Messaging(ctx)
 
 	//hashmap with string index and string value
 	razredi := make(map[string]string)
@@ -34,20 +47,14 @@ func main() {
 		e.ForEach("option", func(opt int, option *colly.HTMLElement) {
 			razredi[option.Text] = option.Attr("value")
 		})
-		getschedule(razredi[razred.izbranrazred])
+
 	})
 	//get class i want from app
-	http.HandleFunc("/setClass", func(w http.ResponseWriter, r *http.Request) {
-		decoder := json.NewDecoder(r.Body)
-		var razred izbranRazred
-		err := decoder.Decode(&razred)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		getschedule(razredi[razred.izbranrazred])
-	})
 
-	http.HandleFunc("/danes", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/danes/", func(w http.ResponseWriter, r *http.Request) {
+
+		razred := strings.TrimPrefix(r.URL.Path, "/danes/")
+		getschedule(razred)
 
 		indexDneva := int(time.Now().Weekday()) - 1
 		var urnikDanes [9]vsebina = [9]vsebina{}
@@ -57,27 +64,40 @@ func main() {
 		sendData(w, r, urnikDanes)
 	})
 
+	http.HandleFunc("/izbranDan/", func(w http.ResponseWriter, r *http.Request) {
+		podatki := strings.TrimPrefix(r.URL.Path, "/izbranDan/")
+		izbranRazred := strings.Split(podatki, "/")[0]
+		izbranDan, err := strconv.Atoi(strings.Split(podatki, "/")[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		getschedule(izbranRazred)
+
+		var izbranUrnik [9]vsebina = [9]vsebina{}
+		for i := 0; i < 9; i++ {
+			izbranUrnik[i] = dnevi[i][izbranDan]
+		}
+		sendData(w, r, izbranUrnik)
+	})
+
 	http.HandleFunc("/allClasses", func(w http.ResponseWriter, r *http.Request) {
 		imenarazredov := []string{}
 		for k := range razredi {
 			imenarazredov = append(imenarazredov, k)
 		}
-		b, _ := json.Marshal(imenarazredov)
+		b, err := json.Marshal(imenarazredov)
+		if err != nil {
+			log.Fatal(err)
+		}
 		fmt.Fprintf(w, string(b))
 	})
 
 	c.Visit("https://www.easistent.com/urniki/5738623c4f3588f82583378c44ceb026102d6bae/razredi/242982")
-	http.ListenAndServeTLS(":443", "/etc/letsencrypt/live/easy-matura.ddns.net/fullchain.pem", "/etc/letsencrypt/live/easy-matura.ddns.net/privkey.pem", nil)
+	fmt.Println("listening on port 443")
+	log.Fatal(http.ListenAndServeTLS(":443", "/etc/letsencrypt/live/easy-matura.ddns.net/fullchain.pem", "/etc/letsencrypt/live/easy-matura.ddns.net/privkey.pem", nil))
 }
 
 func getschedule(razred string) {
-	opt := option.WithCredentialsFile("path/to/serviceAccountKey.json")
-	app, err := firebase.NewApp(context.Background(), nil, opt)
-	if err != nil {
-		fmt.Errorf("error initializing app: %v", err)
-	}
-	ctx := context.Background()
-	client, err := app.Messaging(ctx)
 	urnik := []vsebina{}
 	c := colly.NewCollector()
 	c.OnHTML("table.ednevnik-seznam_ur_teden", func(e *colly.HTMLElement) {
@@ -85,45 +105,48 @@ func getschedule(razred string) {
 			tr.ForEach("table.ednevnik-seznam_ur_teden > tbody > tr > td", func(indextd int, td *colly.HTMLElement) {
 				predmet := td.DOM.Find(".text14").Text()
 				profesor := td.DOM.Find(".text11").Text()
-				neki := vsebina{strings.TrimSpace(predmet), strings.TrimSpace(profesor)}
-				urnik = append(urnik, neki)
+				prebraniPodatki := vsebina{strings.TrimSpace(predmet), strings.TrimSpace(profesor)}
+				urnik = append(urnik, prebraniPodatki)
 				//fmt.Println(strings.TrimSpace(neki.predmet))
 				//fmt.Println(strings.TrimSpace(neki.profesor))
 				//fmt.Println(indextd)
-				dnevi[indextr][indextd] = neki
+				dnevi[indextr-1][indextd] = prebraniPodatki
 			})
 		})
 		//fmt.Println(dnevi)
 
-		for ura := 0; ura < 9; ura++ {
+		/*for ura := 0; ura < 9; ura++ {
 			for dan := 0; dan < 6; dan++ {
 				fmt.Print(dnevi[ura][dan])
 			}
 			fmt.Println()
-		}
+		}*/
 
 		ure := []string{"7.05", "7.50", "8.40", "9.30", "10.20", "11.10", "12.00", "12.50", "13.40"}
 		danasnjiDan := int(time.Now().Weekday()) + 1
-		for i := 1; i < 11; i++ {
+		fmt.Println(danasnjiDan)
+		for i := 1; i < 9; i++ {
+			fmt.Println(i)
 			stringToTime, _ := time.Parse("15.04", ure[i])
 			timeDIFF := stringToTime.Sub(time.Now())
-			time.AfterFunc(timeDIFF, func() {
-				topic := "notification"
-				message := &messaging.Message{
-					Data: map[string]string{
-						"imePredmeta": dnevi[i][danasnjiDan].predmet,
-						"profesor":    dnevi[i][danasnjiDan].profesor,
-					},
-					Topic: topic,
-				}
-				// Send a message to the devices subscribed to the provided topic.
-				response, err := client.Send(ctx, message)
-				if err != nil {
-					log.Fatalln(err)
-				}
-				// Response is a message ID string.
-				fmt.Println("Successfully sent message:", response)
 
+			topic := "notification"
+			message := &messaging.Message{
+				Data: map[string]string{
+					"imePredmeta": dnevi[i][danasnjiDan].predmet,
+					"profesor":    dnevi[i][danasnjiDan].profesor,
+				},
+				Topic: topic,
+			}
+			// Send a message to the devices subscribed to the provided topic.
+			response, err := client.Send(ctx, message)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			// Response is a message ID string.
+			fmt.Println("Successfully sent message:", response)
+
+			time.AfterFunc(timeDIFF, func() {
 			})
 		}
 	})
